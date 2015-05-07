02f16a390fe0006a0477b3af68622d4387640384
ngominhquan <ngominhquan.84@gmail.com>
2015-05-07 16:59:38 +0700

    add jquery

---
+++ TemplateSourceCode/Controllers/JqueryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TemplateSourceCode.Controllers
+{
+    public class JqueryController : Controller
+    {
+        //
+        // GET: /Jquery/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}

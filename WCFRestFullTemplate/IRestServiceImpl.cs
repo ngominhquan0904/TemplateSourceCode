6db782e47001b7357b192d917fa3060a5b321a95
ngominhquan <ngominhquan.84@gmail.com>
2015-05-13 17:06:38 +0700

    afdf

---
+++ WCFRestFullTemplate/IRestServiceImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFRestFullTemplate
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestServiceImpl" in both code and config file together.
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string XMLData(string id);
+        [OperationContract]
+        [WebInvoke(Method = "GET",ResponseFormat = WebMessageFormat.Json,BodyStyle = WebMessageBodyStyle.Wrapped,UriTemplate = "DoWork")]
+        string DoWork();
+    }
+}
---
+++ WCFRestFullTemplate/RestServiceImpl.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WCFRestFullTemplate
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestServiceImpl" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RestServiceImpl.svc or RestServiceImpl.svc.cs at the Solution Explorer and start debugging.
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        public string XMLData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        public string DoWork()
+        {
+            var product = new Product {Name = "aaa", Address = "bbb"};
+            return JsonConvert.SerializeObject(product);
+        }
+    }
+    public class Product
+{
+  public   string Name { get; set; }
+  public string Address { get; set; }
+}
+}
